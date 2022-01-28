ee75a2fe93a18d3772e3ac39e14747eb8be6ab8f
Alycia Nguyen <alycin2@uw.edu>
2022-01-28 03:28:56 -0800

    troubleshooting 2 - ready for testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,30 @@
-import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import App from './App';
 import QuizContent from './QuizContent';
+import {MemoryRouter} from 'react-router-dom';
+import {rest} from 'msw';
+import {setupServer} from 'msw/node';
+
+// testing full app rendering
+test('full app rendering/navigating', () => {
+  render(<App />, {wrapper: MemoryRouter})
+
+  // verify page content for expected route
+  expect(screen.getByText(/Mission Statement/i)).toBeInTheDocument()
+})
 
 // Pulls screen text and asserts if its in the document
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
 
-test("Adding test case 1", () => {
+// test("Adding test case 1", () => {
 
-  const sum = 1 + 1;
+//   const sum = 1 + 1;
 
-  expect(sum).toEqual(2);
+//   expect(sum).toEqual(2);
 
-})
+// })
